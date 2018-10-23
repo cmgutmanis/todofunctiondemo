82bcd406c56aa4d0c12d20c4da46f90ce7c8dfe5
Chris Gutmanis <chris.gutmanis@centare.com>
2018-10-23 16:54:02 -0500

    adding routs

---
+++ TodoFunction/Function1.cs
@@ -0,0 +1,43 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace TodoFunction
+{
+    public static class TodoApi
+    {
+        static List<Todo> todos = new List<Todo>();
+
+        [FunctionName("CreateTodo")]
+        public static async Task<IActionResult> CreateTodo(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "todo")]HttpRequest req, ILogger log)
+        {
+            log.LogInformation("Creating a new todo item");
+            var requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+
+            var input = JsonConvert.DeserializeObject<TodoCreateModel>(requestBody);
+            if (string.IsNullOrEmpty(input.Description))
+                return new BadRequestResult();
+
+            var todo = new Todo { Description = input.Description };
+            todos.Add(todo);
+            return new OkObjectResult(todo);
+        }
+
+        [FunctionName("GetAllTodos")]
+        public static IActionResult GetAllTodos(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route ="todo")]HttpRequest req, ILogger log)
+        {
+            log.LogInformation("Getting All Todos.");
+
+            return new OkObjectResult(todos);
+        }
+    }
+}
---
+++ TodoFunction/TodoModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TodoFunction
+{
+    public class Todo
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        public string Description { get; set; }
+
+        public DateTime CreatedTime { get; set; } = DateTime.Now;
+
+        public bool IsCompleted { get; set; }
+    }
+
+    public class TodoCreateModel
+    {
+        public string Description { get; set; }
+    }
+
+    public class TodoUpdateModel
+    {
+        public string Description { get; set; }
+
+        public bool IsCompleted { get; set; }
+    }
+}
