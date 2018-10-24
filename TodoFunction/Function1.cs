7e92f88cc042e6e31d4f465adcd48309855c1cd7
Chris Gutmanis <chris.gutmanis@centare.com>
2018-10-24 09:26:30 -0500

    adding methods

--- TodoFunction/Function1.cs
+++ TodoFunction/Function1.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace TodoFunction
 {
@@ -38,6 +39,40 @@ namespace TodoFunction
             log.LogInformation("Getting All Todos.");
 
             return new OkObjectResult(todos);
+        }
+
+        [FunctionName("GetTodo")]
+        public static IActionResult GetTodo(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "todo/{id}")]HttpRequest req, ILogger log, string id)
+        {
+            log.LogInformation("Getting todo");
+            var todo = todos.FirstOrDefault(t => t.Id == id);
+
+            return new OkObjectResult(todo);
+        }
+
+        [FunctionName("UpdateTodo")]
+        public static async Task<IActionResult> UpdateTodo(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "put", Route = "todo/{id}")]HttpRequest req, ILogger log, string id)
+        {
+            log.LogInformation("updating todo");
+            var requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+
+            var input = JsonConvert.DeserializeObject<TodoUpdateModel>(requestBody);
+            var todo = todos.FirstOrDefault(t => t.Id == id);
+            todo.IsCompleted = input.IsCompleted;
+            todo.Description = input.Description;
+
+            return new OkObjectResult(todo);
+        }
+
+        [FunctionName("DeleteTodo")]
+        public static IActionResult DeleteTodo(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "delete", Route ="todo/{id}")]HttpRequest req, ILogger log, string id)
+        {
+            todos.Remove(todos.FirstOrDefault(t => t.Id == id));
+
+            return new OkResult();
         }
     }
 }
